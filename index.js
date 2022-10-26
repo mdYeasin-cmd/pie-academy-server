1a7bc6f18143b1e6ae62642c01b57a4b8531ce98
mdYeasin-cmd <yeasin200011@gmail.com>
2022-10-26 17:06:02 +0600

    Dynamic api setup for get single course data using id

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 });
 
+app.get('/courseDetails/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const course = courses.find(course => course.course_id === id);
+    res.send(course);
+});
+
 app.listen(port, () => {
     console.log('Pie Academy Sever is running on port', port);
 });
