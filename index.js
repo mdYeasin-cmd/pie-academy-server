43c0aebf5d355d4d19275c3f5799cc06eef1689c
mdYeasin-cmd <yeasin200011@gmail.com>
2022-10-26 10:07:35 +0600

    C:/Program Files/Git/courses api is added

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('Pie Academy server is running');
-})
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
 
 app.listen(port, () => {
     console.log('Pie Academy Sever is running on port', port);
-})
+});
